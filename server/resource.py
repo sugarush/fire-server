086b33dff1c104a7166de6fc95b080e484857df5
Paul Severance <paul@sugarush.io>
2020-04-20 22:18:08 -0400

    remove unused comments

--- server/resource.py
+++ server/resource.py
@@ -6,11 +6,5 @@ from models.user import User
 
 
 server.blueprint(Authentication.resource(url_prefix='/v1'))
-'''
-Blah
-'''
 
 server.blueprint(User.resource(url_prefix='/v1', pubsub=True))
-'''
-Blah
-'''
